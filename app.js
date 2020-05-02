eaeaa10d4071f53b475e2823944d8c20fde5be6d
G Vamshi Krishna <vamshiginna1606@gmail.com>
2020-05-03 03:31:53 +0530

    Added Middleware

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const PORT = 5000
 app.get('/',(req,res)=>{
     res.send("hello world")
 })
-
+app.get('/about',customMiddleware,(req,res)=>{
+    console.log("about")
+    res.send("About page")
+})
 app.listen(PORT,()=>{
     console.log("server is running on", PORT)
 })
